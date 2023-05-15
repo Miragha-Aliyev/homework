154a7b637d0a0a6bef4563eda267df52ff9af024
unknown <Miragha-Aliyev>
2023-05-15 11:23:23 +0400

    my homework

---
+++ main.js
@@ -0,0 +1,100 @@
+// ====================================
+// ===========homework 1===============
+// ====================================
+
+// let myArrey = [1,15,13,26,95,23,0,59]
+
+// console.log (myArrey.length)
+// console.log (myArrey[0])
+
+// for(let i=0; i<myArrey.length; i++) {
+//     // console.log (myArrey[i]) 
+//     let number = myArrey [i]
+
+//     console.log(number)
+// }
+
+
+// ====================================
+// ===========homework 2===============
+// ====================================
+
+// function calcPerimeter(){
+//     let width = 2
+//     let length = 4
+    
+//     let perimeter =(width+length)*2
+    
+//     console.log(perimeter)
+// }
+
+// calcPerimeter ()
+
+
+
+// ====================================
+// ===========homework 3===============
+// ====================================
+
+// function add(width, length){
+//     console.log(width, length)
+
+// }
+// add(4, 8)
+
+
+// function calcArea(){
+//     let width = 4
+//     let length = 8
+
+//     let area =width*length
+
+//     console.log(area)
+// }
+
+// calcArea()
+
+
+
+// ====================================
+// ===========homework 4===============
+// ====================================
+
+
+// function calcCircleArea(){
+ 
+// let diametr = 8
+// let radius = 4
+// let pi= 3.14
+
+// let area = pi*radius**2
+
+
+// console.log(area)
+// }
+
+// calcCircleArea()
+
+
+
+// ====================================
+// ===========homework 5===============
+// ====================================
+
+// let myArrey = [1,15,13,26,95,23,0,59]
+
+
+// let sum = 0
+
+
+// for (let i=0; i<myArrey.length; i++){
+ 
+// let num = myArrey[i]
+
+// if (num % 2 == 1){
+//     sum += num 
+// }
+
+//  }
+
+//  console.log(sum)
